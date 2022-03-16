47dfb4f95617a1f52d944680e230c7c5565ade90
Irina Nicolai <irina.nic.yu@gmail.com>
2022-03-16 18:42:19 +0100

    Add styles for ProductList component

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,16 +1,22 @@
+import React from "react";
+import styles from './ProductList.module.scss';
+
 const ProductList = ({products}) => {
 
     const currency = '€';
 
     return(
-        <ul>
+        <ul className={styles.productList}>
             {products.map((product) => {
                 const {id, name, price, imageURL} = product;
                 return (
-                    <li key={id}>
-                        <img src={imageURL} alt={name}/>
-                        <h2>{name}</h2>
-                        <p>{`${price} ${currency}`}</p>
+                    <li key={id} className={styles.productList__item}>
+                        <img src={imageURL} alt={name} className={styles.productList__img}/>
+                        <div className={styles.productList__info}>
+                            <h2 className={styles.productList__name}>{name}</h2>
+                            <p className={styles.productList__price}>{`${price} ${currency}`}</p>
+                        </div>
+
                     </li>
                 )
             })}
