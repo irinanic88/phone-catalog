684b6158f9062d5318a841b68635069e5ecaf9e9
Irina Nicolai <irina.nic.yu@gmail.com>
2022-03-17 15:41:09 +0100

    Fix ProductList tests according to routing

--- src/components/ProductList/ProductList.test.js
+++ src/components/ProductList/ProductList.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
@@ -9,12 +10,20 @@ Enzyme.configure({adapter: new Adapter()});
 
 describe('ProductList', () => {
     it('should render', () => {
-        const wrapper = mount(<ProductList products={productsMock} />);
+        const wrapper = mount(
+            <BrowserRouter>
+                <ProductList />
+            </BrowserRouter>
+        );
         expect(wrapper.find('[data-id="product-list"]').length).toBe(1);
     });
 
     it('should render a given amount of list items', () => {
-        const wrapper = mount(<ProductList />);
+        const wrapper = mount(
+            <BrowserRouter>
+                <ProductList />
+            </BrowserRouter>
+        );
         expect(wrapper.find('[data-id="product-list"]').children().length).toBe(products.length);
     })
 })
