d8637f61ad10aca473e294449d6edcd8a403704b
Irina Nicolai <irina.nic.yu@gmail.com>
2022-03-18 17:01:40 +0100

    Fix productCatalogReducer tests

--- src/store/reducers/productCatalogReducer/productCatalogReducer.test.js
+++ src/store/reducers/productCatalogReducer/productCatalogReducer.test.js
@@ -1,12 +1,17 @@
 import productCatalogReducer from './productCatalogReducer';
-import { LOAD_PRODUCT_LIST } from '../../actionTypes';
+import { LOAD_PRODUCT_LIST, SUCCESS, FAILURE } from '../../actionTypes';
 
 const mockState = [];
 
-const mockActionWithLoadProductListType = {
-    type: LOAD_PRODUCT_LIST,
+const mockActionWithSuccessType = {
+    type: LOAD_PRODUCT_LIST + SUCCESS,
     data: [1, 2, 3],
 }
+
+const mockActionWithFailureType = {
+    type: LOAD_PRODUCT_LIST + FAILURE,
+}
+
 const mockActionWithDifferentType = {
     type: 'SOME_TYPE',
 }
@@ -14,8 +19,12 @@ const mockActionWithDifferentType = {
 const mockActionWithoutType = {};
 
 describe('productCatalogReducer', () => {
-    test('should return new array with data if action type is LOAD_PRODUCT_LIST', () => {
-        expect(productCatalogReducer(mockState, mockActionWithLoadProductListType)).toEqual([1, 2, 3]);
+    test('should return new array with data if action type is LOAD_PRODUCT_LIST + SUCCESS', () => {
+        expect(productCatalogReducer(mockState, mockActionWithSuccessType)).toEqual([1, 2, 3]);
+    });
+
+    test('should return state if action type is is LOAD_PRODUCT_LIST + FAILURE', () => {
+        expect(productCatalogReducer(mockState, mockActionWithFailureType)).toEqual(mockState);
     });
 
     test('should return state if action type is not LOAD_PRODUCT_LIST', () => {
